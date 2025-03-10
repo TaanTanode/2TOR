b1d3ece124bf87f2b3a4f815ce32e2c386942282
TaanTanode <taantanode1428@gmail.com>
2025-03-10 18:35:36 +0700

    first commit

---
+++ server/controllers/auth.js
@@ -0,0 +1,54 @@
+
+
+exports.register = async(req,res)=>{
+    //code
+    try{
+        //code
+        const { email, password } = req.body
+
+        //validate body
+        if(!email){
+            return res.status(400).json({ message: 'Email is required!!!'})
+        }
+        if(!password){
+            return res.status(400).json({ message: 'Password is required!!!'})
+        }
+
+        //check email in DB
+        
+
+
+
+        console.log(email, password)
+        res.send('Hello Register in Controller')
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Errors" })
+    }
+  
+}
+
+exports.login = async(req,res)=>{
+    try{
+        //code
+        res.send("Hello Login in Controller")
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Errors"})
+    }
+    
+}
+
+exports.currentUser = async(req,res) =>{
+    try{
+        //code
+        res.send("Hello Current User")
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Errors"})
+    }
+}
+
---
+++ server/controllers/category.js
@@ -0,0 +1,35 @@
+
+exports.create = async(req,res)=>{
+    try{
+        //code
+        res.send("Hello Category")
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+exports.list = async(req,res)=>{
+    try{
+        //code
+        res.send("Hello Category List")
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Error" })
+    }
+}
+
+exports.remove = async(req,res)=>{
+    try{
+        //code
+        const { id } = req.params
+        console.log(id)
+        res.send("Hello Category Remove")
+    }catch(err){
+        //error
+        console.log(err)
+        res.status(500).json({ message: "Server Error" })
+    }
+}
