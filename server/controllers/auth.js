80935904e2742053a7bd0e952a97001d03211454
TaanTanode <taantanode1428@gmail.com>
2025-03-17 17:24:11 +0700

    check email,password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -51,6 +51,26 @@ exports.register = async(req,res)=>{
 exports.login = async(req,res)=>{
     try{
         //code
+        const { email, password } = req.body
+
+        //step 1 check email ก่อนว่าที่ส่งมาถูกมั้ย ได้เปิดการใช้งานมั้ย
+        const user =await prisma.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+        if(!user || !user.enabled){
+            return res.status(400).json({ message: 'User Not Found or Not Enabled' })
+        }
+        //step 2 check password
+        const isMatch = await bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({ message: 'Password Invalid!!!'})
+        }
+        //step 3 chech payload
+        //step 4 generate token
+
+        console.log(email, password)
         res.send("Hello Login in Controller")
     }catch(err){
         //error
