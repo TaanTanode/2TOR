a475791df5475565c3edefb50a5d76363950b98a
TaanTanode <taantanode1428@gmail.com>
2025-03-27 21:21:43 +0700

    update auth.js for secure

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,12 +2,14 @@ const express = require('express')
 const router = express.Router()
 //import controller
 const { register, login, currentUser } = require('../controllers/auth')
+//import middleware
 
+const { authCheck, adminCheck } = require('../middlewares/authCheck')
 
 
 router.post('/register', register)
-router.post('/login' , login)
-router.post('/current-user', currentUser)
-router.post('/current-admin',currentUser)
+router.post('/login', login)
+router.post('/current-user', authCheck, currentUser)
+router.post('/current-admin', authCheck, adminCheck, currentUser)
 
-module.exports =  router
+module.exports = router
